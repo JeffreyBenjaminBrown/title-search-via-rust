466ad02cc1d853e28f8e0ccb26c7e2c41b4f3926
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2025-04-02 16:44:12 -0500

    docs, docker tweaks

---
+++ README.md
@@ -0,0 +1,11 @@
+# USAGE
+
+Start the docker container (using something like the
+`docker run` command in `docker.sh`).
+Enter it                   (using something like the
+`docker exec` command in `docker.sh`).
+Run `cd` to go to the home folder (`/home/ubuntu`).
+
+Pick an executable from `Cargo.toml`.
+Supposing for this paragraph you picked `via-tantivy`,
+then you would run `cargo run --bin via-tantivy`.
--- docker.sh
+++ docker.sh
@@ -1,10 +1,9 @@
 exit # This is not a script, just snippets.
 
-CONTAINER_NAME=rust-typedb2
+CONTAINER_NAME=title-search
 
 docker exec -it $CONTAINER_NAME bash
 docker stop $CONTAINER_NAME && docker rm $CONTAINER_NAME
-
 
 STARTING_AT=$(date)
 echo $(date)
@@ -16,12 +15,13 @@ docker tag jeffreybbrown/hode:new jeffreybbrown/hode:latest
 docker tag jeffreybbrown/hode:new jeffreybbrown/hode:$DOCKER_IMAGE_SUFFIX
 docker rmi jeffreybbrown/hode:new
 
-docker run --name $CONTAINER_NAME -it -d        \
-  -v /home/jeff/hodal/title-search:/home/ubuntu \
-  -p 1729:1729                                  \
-  --platform linux/amd64                        \
-  --user 1000:1000                              \
-  jeffreybbrown/hode:new # CAREFUL! new? latest?
+NATIVE=/home/jeff/hodal/connection-demos/title-search
+docker run --name $CONTAINER_NAME -it -d \
+  -v $NATIVE:/home/ubuntu                \
+  -p 1729:1729                           \
+  --platform linux/amd64                 \
+  --user 1000:1000                       \
+  jeffreybbrown/hode:latest # CAREFUL! new? latest?
 
 docker push jeffreybbrown/hode:$DOCKER_IMAGE_SUFFIX
 docker push jeffreybbrown/hode:latest
